595962cd86c3a607200864093da0689eb6681fe4
ipinnxitin <thangtran882110@gmail.com>
2025-06-23 15:38:19 +0700

    Allow all requests

--- src/main/java/com/ipin/identity/security/SecurityConfig.java
+++ src/main/java/com/ipin/identity/security/SecurityConfig.java
@@ -2,7 +2,7 @@ package com.ipin.identity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+// import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,20 +18,19 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 @EnableMethodSecurity
 public class SecurityConfig {
 
-    String[] PUBLIC_ENDPOINTS = {"/user", "/auth/login", "/auth/introspect", "/auth/logout", "/auth/refresh"};
+    // String[] PUBLIC_ENDPOINTS = {"/user", "/auth/login", "/auth/introspect", "/auth/logout", "/auth/refresh"};
 
     CustomJwtDecoder customJwtDecoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> 
-            auth.requestMatchers(HttpMethod.POST, PUBLIC_ENDPOINTS).permitAll()
-            .anyRequest().authenticated())
-        .csrf(csrf -> csrf.disable())
-        .oauth2ResourceServer(oauth2 -> 
-            oauth2.jwt(jwtConfigurer -> 
-                jwtConfigurer.decoder(customJwtDecoder)
-                .jwtAuthenticationConverter(jwtAuthenticationConverter())));
+            auth.anyRequest().permitAll())
+        .csrf(csrf -> csrf.disable());
+        // .oauth2ResourceServer(oauth2 -> 
+        //     oauth2.jwt(jwtConfigurer -> 
+        //         jwtConfigurer.decoder(customJwtDecoder)
+        //         .jwtAuthenticationConverter(jwtAuthenticationConverter())));
 
         return http.build();
     }
